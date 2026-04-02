a88daf0353eec2853aead2c64d8e91396ab47aa8
imluffy000 <prasanthchangala000@gmail.com>
2026-04-02 23:44:09 +0530

    feat: redesign portfolio hero and about sections with new layout and profile imagery

--- script.js
+++ script.js
@@ -34,3 +34,59 @@ let navLinks = document.querySelectorAll(".menu li a");
 navLinks.forEach((link) => {
   link.addEventListener("click", hideNavMenu);
 });
+
+// --- Stars Background Animation ---
+const canvas = document.getElementById("starsCanvas");
+const ctx = canvas.getContext("2d");
+
+let stars = [];
+const numStars = 150;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+class Star {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 1.5;
+    this.speedY = Math.random() * 0.3 + 0.1;
+    this.speedX = (Math.random() - 0.5) * 0.1;
+    // Dark color since we are using light gradient from the component
+    this.color = "rgba(0, 0, 0, 0.6)"; 
+  }
+  update() {
+    this.y -= this.speedY; // Stars moving up conceptually
+    this.x += this.speedX;
+    if (this.y < 0) {
+      this.y = canvas.height;
+      this.x = Math.random() * canvas.width;
+    }
+    if (this.x < 0) this.x = canvas.width;
+    if (this.x > canvas.width) this.x = 0;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+for (let i = 0; i < numStars; i++) {
+  stars.push(new Star());
+}
+
+function animateStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  stars.forEach(star => {
+    star.update();
+    star.draw();
+  });
+  requestAnimationFrame(animateStars);
+}
+animateStars();
